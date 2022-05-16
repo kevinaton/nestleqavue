cd573b5e31bbb4da1ec0b9dfc041d69dcb167b61
j-borbon <jacqueline.borbon@applusvelosi.com>
2022-05-16 18:17:45 +0800

    Task 11311: Create conversion script and convert the data

---
+++ MigrationScripts/00_HRD_11311_CreateConversionSript to conver the data - Updated.sql
@@ -0,0 +1,342 @@
+USE [HRD]
+GO
+
+
+IF OBJECT_ID('HRD', 'U') IS  NULL 
+BEGIN
+CREATE TABLE [dbo].[HRD](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Plant] [nchar](4) NULL,
+	[Originator] [nvarchar](50) NULL,
+	[HRD] [bit] NULL,
+	[DayCode] [nvarchar](7) NULL,
+	[Line] [nvarchar](50) NULL,
+	[Shift] [nvarchar](50) NULL,
+	[LCL] [nvarchar](50) NULL,
+	[Schedule] [nvarchar](50) NULL,
+	[HourCode] [nvarchar](100) NULL,
+	[Cases] [int] NULL,
+	[UnpalletizedProductHeld] [bit] NULL,
+	[RejectOrNinePack] [bit] NULL,
+	[ReworkOutsideNormalProduction] [bit] NULL,
+	[ContactOtherPlant] [bit] NULL,
+	[GLOBENum] [nvarchar](10) NULL,
+	[ShortDescription] [nvarchar](75) NULL,
+	[Problem] [nvarchar](max) NULL,
+	[ReworkInstructions] [nvarchar](max) NULL,
+	[ReworkApproved] [bit] NULL,
+	[ReworkApprovedBy] [nvarchar](50) NULL,
+	[ReworkStarted] [datetime] NULL,
+	[ReworkComplete] [datetime] NULL,
+	[ReworkCompletedBy] [nvarchar](50) NULL,
+	[CauseMan] [bit] NULL,
+	[CauseMethod] [bit] NULL,
+	[CauseMaterial] [bit] NULL,
+	[CauseMachine] [bit] NULL,
+	[TLforFU] [nvarchar](50) NULL,
+	[Classification] [nvarchar](50) NULL,
+	[FTQCases] [int] NULL,
+	[NonFTQCases] [int] NULL,
+	[DCDate] [datetime] NULL,
+	[DCUser] [nvarchar](50) NULL,
+	[FCDate] [datetime] NULL,
+	[FCUser] [nvarchar](50) NULL,
+	[OtherHRDAffected] [bit] NULL,
+	[OtherHRDNum] [nvarchar](50) NULL,
+	[DateHeld] [datetime] NULL,
+	[HoldCategory] [nvarchar](50) NULL,
+	[HoldSubCategory] [nvarchar](50) NULL,
+	[CostofProductonHold] [money] NULL,
+	[DateofDisposition] [datetime] NULL,
+	[Clear] [int] NULL,
+	[Scrap] [int] NULL,
+	[ThriftStore] [int] NULL,
+	[Samples] [int] NULL,
+	[Cancelled] [bit] NULL,
+	[Complete] [bit] NULL,
+	[HRDCompletedBy] [nvarchar](50) NULL,
+	[AllCasesAccountedFor] [bit] NULL,
+	[DateReleased] [datetime] NULL,
+	[TestingCost] [money] NULL,
+	[LaborHours] [numeric](10, 4) NULL,
+	[GSTDRequired] [bit] NULL,
+	[HighRisk] [bit] NULL,
+	[PhysicalIsolationNeeded] [bit] NULL,
+	[Physical5DaysGSTD] [bit] NULL,
+	[LastChange] [datetime] NULL,
+	[LastChangeUser] [nvarchar](100) NULL,
+	[ReworkApprovedDate] [datetime] NULL,
+	[CodingType] [nvarchar](20) NULL,
+	[CodingDetails] [nvarchar](10) NULL,
+	[YearHeld] [nvarchar](4) NULL,
+	[QAComments] [nvarchar](max) NULL,
+	[SecondaryNotification] [bit] NULL,
+ CONSTRAINT [PK_HRD] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 90, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+END
+GO
+
+IF OBJECT_ID('DropDownType', 'U') IS  NULL 
+BEGIN
+CREATE TABLE [dbo].[DropDownType](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](100) NOT NULL,
+ CONSTRAINT [PK_DropDownType] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+END
+GO
+
+
+IF OBJECT_ID('DropDownItem', 'U') IS  NULL 
+BEGIN
+CREATE TABLE [dbo].[DropDownItem](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[DropDownTypeId] [int] NOT NULL,
+	[Value] [nvarchar](50) NOT NULL,
+	[SortOrder] [smallint] NOT NULL,
+	[IsActive] [bit] NOT NULL,
+    FOREIGN KEY (DropDownTypeId) REFERENCES DropDownType(Id),
+ CONSTRAINT [PK_DropDownItem] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+END
+GO
+
+
+IF OBJECT_ID('HRDPO', 'U') IS  NULL 
+BEGIN
+CREATE TABLE [dbo].[HRDPO](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[HRDId] [int] NOT NULL,
+	[PONumber] [nvarchar](10) NOT NULL,
+    FOREIGN KEY (HRDId) REFERENCES HRD(Id),
+ CONSTRAINT [PK_HRDPO] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+END
+GO
+
+IF OBJECT_ID('HRDFC', 'U') IS  NULL 
+BEGIN
+CREATE TABLE [dbo].[HRDFC](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[HRDId] [int] NOT NULL,
+	[Location] [nvarchar](10) NOT NULL,
+	[NumberOfCases] [int] NOT NULL,
+    FOREIGN KEY (HRDId) REFERENCES HRD(Id),
+ CONSTRAINT [PK_HRDFC] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+END
+GO
+
+IF OBJECT_ID('HRDDC', 'U') IS  NULL 
+BEGIN
+CREATE TABLE [dbo].[HRDDC](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[HRDId] [int] NOT NULL,
+	[Location] [nvarchar](10) NOT NULL,
+	[NumberOfCases] [int] NOT NULL,
+    FOREIGN KEY (HRDId) REFERENCES HRD(Id),
+ CONSTRAINT [PK_HRDDC] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+END
+GO
+
+IF OBJECT_ID('LaborCost', 'U') IS  NULL 
+BEGIN
+CREATE TABLE [dbo].[LaborCost](
+	[Year] [char](4) NOT NULL,
+	[LaborCost] [smallmoney] NULL,
+ CONSTRAINT [PK_LaborCost] PRIMARY KEY CLUSTERED 
+(
+	[Year] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 95, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+END
+GO
+
+IF OBJECT_ID('Product', 'U') IS  NULL 
+BEGIN
+CREATE TABLE [dbo].[Product](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Year] [nchar](4) NOT NULL,
+	[GPN] [nvarchar](10) NOT NULL,
+	[Description] [nvarchar](100) NULL,
+	[CostPerCase] [money] NULL,
+	[Country] [nchar](2) NULL,
+	[NoBBDate] [bit] NULL,
+	[Holiday] [bit] NULL,
+ CONSTRAINT [PK_Product] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+END
+GO
+ALTER TABLE [dbo].[Product] ADD CONSTRAINT [DF_Product_Year]  DEFAULT ((0)) FOR [Year]
+GO
+ALTER TABLE [dbo].[Product] ADD CONSTRAINT [DF_Product_GPN]  DEFAULT ((0)) FOR [GPN]
+GO
+
+
+IF OBJECT_ID('HRDNotes', 'U') IS  NULL 
+BEGIN
+CREATE TABLE [dbo].[HRDNotes](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[HRDId] [int] NULL,
+	[Category] [nvarchar](50) NULL,
+	[Description] [nvarchar](max) NULL,
+	[UserID] [nvarchar](50) NULL,
+	[Date] [datetime] NULL,
+	[FileName] [nvarchar](400) NULL,
+	[Path] [nvarchar](400) NULL,
+	[Size] [nvarchar](20) NULL,
+    FOREIGN KEY (HRDId) REFERENCES HRD(Id),
+ CONSTRAINT [PK_HRDNotes] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 95, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+END
+GO
+
+IF OBJECT_ID('Settings', 'U') IS  NULL 
+BEGIN
+CREATE TABLE [dbo].[Settings](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](25) NULL,
+	[Description] [nvarchar](255) NULL,
+	[Value] [nvarchar](255) NULL,
+	[Type] [nvarchar](10) NULL,
+ CONSTRAINT [PK_Settings] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 95, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+END
+GO
+
+IF OBJECT_ID('Security', 'U') IS  NULL 
+BEGIN
+CREATE TABLE [dbo].[Security](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Type] [nvarchar](5) NULL,
+	[Name] [nvarchar](50) NULL,
+	[Administrator] [bit] NULL,
+	[QAManager] [bit] NULL,
+	[QATGrp] [bit] NULL,
+	[QAProdRelGrp] [bit] NULL,
+	[ProdTLGrp] [bit] NULL,
+	[ReworkMembersGrp] [bit] NULL,
+	[PhysicalGrp] [bit] NULL,
+	[Physical5Grp] [bit] NULL,
+	[GSTDNotificationGrp] [bit] NULL,
+	[DeleteHRD] [bit] NULL,
+	[EditHRDApproveRework] [bit] NULL,
+	[ProdMaintenanceList] [bit] NULL,
+	[GSTDGrp] [bit] NULL,
+	[BUMGrp] [bit] NULL,
+	[LaborCostList] [bit] NULL,
+	[Notification] [bit] NULL,
+ CONSTRAINT [PK_Security] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 95, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+END
+GO
+
+IF OBJECT_ID('TestCost', 'U') IS  NULL 
+BEGIN
+CREATE TABLE [dbo].[TestCost](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Year] [nvarchar](50) NULL,
+	[TestName] [nvarchar](50) NULL,
+	[TestCost] [money] NULL,
+ CONSTRAINT [PK_TestCost] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 90, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+END
+GO
+
+IF OBJECT_ID('HRDTestCost', 'U') IS  NULL 
+BEGIN	
+CREATE TABLE [dbo].[HRDTestCost](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[HRDId] [int] NOT NULL,
+	[TestName] [nvarchar](50) NULL,
+	[Qty] [int] NULL,
+	[Cost] [money] NULL,
+    FOREIGN KEY (HRDId) REFERENCES HRD(Id),
+ CONSTRAINT [PK_HRDTestCost] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 90, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+END
+GO
+
+
+IF OBJECT_ID('Users', 'U') IS  NULL 
+BEGIN
+CREATE TABLE [dbo].[Users](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[UserID] [nvarchar](50) NULL,
+	[Name] [nvarchar](100) NULL,
+ CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 90, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+END
+GO
+ALTER TABLE [dbo].[Security] ADD  CONSTRAINT [DF_Security_Administrator]  DEFAULT ((0)) FOR [Administrator]
+GO
+ALTER TABLE [dbo].[Security] ADD  CONSTRAINT [DF_Security_QAManager]  DEFAULT ((0)) FOR [QAManager]
+GO
+ALTER TABLE [dbo].[Security] ADD  CONSTRAINT [DF_Security_QATGrp]  DEFAULT ((0)) FOR [QATGrp]
+GO
+ALTER TABLE [dbo].[Security] ADD  CONSTRAINT [DF_Security_QAProdRelGrp]  DEFAULT ((0)) FOR [QAProdRelGrp]
+GO
+ALTER TABLE [dbo].[Security] ADD  CONSTRAINT [DF_Security_ProdTLGrp]  DEFAULT ((0)) FOR [ProdTLGrp]
+GO
+ALTER TABLE [dbo].[Security] ADD  CONSTRAINT [DF_Security_ReworkMembersGrp]  DEFAULT ((0)) FOR [ReworkMembersGrp]
+GO
+ALTER TABLE [dbo].[Security] ADD  CONSTRAINT [DF_Security_PhysicalGrp]  DEFAULT ((0)) FOR [PhysicalGrp]
+GO
+ALTER TABLE [dbo].[Security] ADD  CONSTRAINT [DF_Security_Physical5Grp]  DEFAULT ((0)) FOR [Physical5Grp]
+GO
+ALTER TABLE [dbo].[Security] ADD  CONSTRAINT [DF_Security_GSTDNotificationGrp]  DEFAULT ((0)) FOR [GSTDNotificationGrp]
+GO
+ALTER TABLE [dbo].[Security] ADD  CONSTRAINT [DF_Security_DeleteHRD]  DEFAULT ((0)) FOR [DeleteHRD]
+GO
+ALTER TABLE [dbo].[Security] ADD  CONSTRAINT [DF_Security_EditHRDApproveRework]  DEFAULT ((0)) FOR [EditHRDApproveRework]
+GO
+ALTER TABLE [dbo].[Security] ADD  CONSTRAINT [DF_Security_ProdMaintenanceList]  DEFAULT ((0)) FOR [ProdMaintenanceList]
+GO
+ALTER TABLE [dbo].[Security] ADD  CONSTRAINT [DF_Security_GSTDGrp]  DEFAULT ((0)) FOR [GSTDGrp]
+GO
+ALTER TABLE [dbo].[Security] ADD  CONSTRAINT [DF_Security_BUMGrp]  DEFAULT ((0)) FOR [BUMGrp]
+GO
+ALTER TABLE [dbo].[Security] ADD  CONSTRAINT [DF_Security_http://localhost:61188/Playground/HRD/ProductMaintenance.aspx]  DEFAULT ((0)) FOR [LaborCostList]
+GO
+ALTER TABLE [dbo].[Security] ADD  CONSTRAINT [DF_Security_Notification]  DEFAULT ((0)) FOR [Notification]
+GO
---
+++ MigrationScripts/01_All_Insert_Statement.sql
@@ -0,0 +1,368 @@
+USE HRD
+
+SET IDENTITY_INSERT dbo.HRD ON 
+INSERT INTO dbo.HRD 
+	(
+		[Id],
+		[Plant],
+		[Originator],
+		[HRD],
+		[DayCode],
+		[Line],
+		[Shift],
+		[LCL],
+		[Schedule],
+		[HourCode],
+		[Cases],
+		[UnpalletizedProductHeld],
+		[RejectOrNinePack],
+		[ReworkOutsideNormalProduction],
+		[ContactOtherPlant],
+		[GLOBENum],
+		[ShortDescription],
+		[Problem],
+		[ReworkInstructions],
+		[ReworkApproved],
+		[ReworkApprovedBy],
+		[ReworkStarted],
+		[ReworkComplete],
+		[ReworkCompletedBy],
+		[CauseMan],
+		[CauseMethod],
+		[CauseMaterial],
+		[CauseMachine],
+		[TLforFU],
+		[Classification],
+		[FTQCases],
+		[NonFTQCases],
+		[DCDate],
+		[DCUser],
+		[FCDate],
+		[FCUser],
+		[OtherHRDAffected],
+		[OtherHRDNum],
+		[DateHeld],
+		[HoldCategory],
+		[HoldSubCategory],
+		[CostofProductonHold],
+		[DateofDisposition],
+		[Clear],
+		[Scrap],
+		[ThriftStore],
+		[Samples],
+		[Cancelled],
+		[Complete],
+		[HRDCompletedBy],
+		[AllCasesAccountedFor],
+		[DateReleased],
+		[TestingCost],
+		[LaborHours],
+		[GSTDRequired],
+		[HighRisk],
+		[PhysicalIsolationNeeded],
+		[Physical5DaysGSTD],
+		[LastChange],
+		[LastChangeUser],
+		[ReworkApprovedDate],
+		[CodingType],
+		[CodingDetails],
+		[YearHeld],
+		[QAComments],
+		[SecondaryNotification]
+	)
+SELECT 
+		[AutoNum],
+		[Plant],
+		[Originator],
+		[HRD],
+		[DayCode],
+		[Line],
+		[Shift],
+		[LCL],
+		[Schedule],
+		[HourCode],
+		[Cases],
+		[UnpalletizedProductHeld],
+		[RejectOrNinePack],
+		[ReworkOutsideNormalProduction],
+		[ContactOtherPlant],
+		[GLOBENum],
+		[ShortDescription],
+		[Problem],
+		[ReworkInstructions],
+		[ReworkApproved],
+		[ReworkApprovedBy],
+		[ReworkStarted],
+		[ReworkComplete],
+		[ReworkCompletedBy],
+		[CauseMan],
+		[CauseMethod],
+		[CauseMaterial],
+		[CauseMachine],
+		[TLforFU],
+		[Classification],
+		[FTQCases],
+		[NonFTQCases],
+		[DCDate],
+		[DCUser],
+		[FCDate],
+		[FCUser],
+		[OtherHRDAffected],
+		[OtherHRDNum],
+		[DateHeld],
+		[HoldCategory],
+		[HoldSubCategory],
+		[CostofProductonHold],
+		[DateofDisposition],
+		[Clear],
+		[Scrap],
+		[ThriftStore],
+		[Samples],
+		[Cancelled],
+		[Complete],
+		[HRDCompletedBy],
+		[AllCasesAccountedFor],
+		[DateReleased],
+		[TestingCost],
+		[LaborHours],
+		[GSTDRequired],
+		[HighRisk],
+		[PhysicalIsolationNeeded],
+		[Physical5DaysGSTD],
+		[LastChange],
+		[LastChangeUser],
+		[ReworkApprovedDate],
+		[CodingType],
+		[CodingDetails],
+		[YearHeld],
+		[QAComments],
+		[SecondaryNotification]
+FROM OldHRD.dbo.HRD oldTable
+		WHERE oldTable.AutoNum NOT IN (SELECT ID FROM dbo.HRD)
+
+SET IDENTITY_INSERT dbo.HRD OFF
+GO
+
+/*********************************************/
+/***** HRDPO  ********/
+/*********************************************/
+INSERT INTO dbo.HRDPO(HRDId, PONumber)
+SELECT AutoNum, PONumber
+FROM 
+   (SELECT AutoNum, PO1, PO2
+   FROM OldHRD.dbo.HRD) ot
+UNPIVOT
+   (
+	PONumber For PONumbers IN (PO1, PO2)
+   )AS unpvt;
+GO
+
+/*********************************************/
+/***** HRDFC  ********/
+/*********************************************/
+INSERT INTO dbo.HRDFC(HRDId, [Location], NumberOfCases)
+SELECT AutoNum, SUBSTRING([Location], 3, DATALENGTH([Location])- 2) as [Location], NumberOfCases
+FROM 
+   (SELECT AutoNum, FC5070, FC5702, FC5847, FC5687, FC5222,
+			CAST(SUBSTRING(FC5222Dest, PATINDEX('%[0-9]%', FC5222Dest), PATINDEX('%[0-9][^0-9]%', FC5222Dest + 't') - PATINDEX('%[0-9]%', 
+                    FC5222Dest) + 1) as int) AS FC5222Dest
+			,FCInTransit, FCXC, FCLX,
+			CAST(SUBSTRING(FCLXDest, PATINDEX('%[0-9]%', FCLXDest), PATINDEX('%[0-9][^0-9]%', FCLXDest + 't') - PATINDEX('%[0-9]%', 
+                    FCLXDest) + 1) as int) AS FCLXDest
+			,FCXD,
+			CAST(SUBSTRING(FCXDDest, PATINDEX('%[0-9]%', FCXDDest), PATINDEX('%[0-9][^0-9]%', FCXDDest + 't') - PATINDEX('%[0-9]%', 
+                    FCXDDest) + 1) as int) AS FCXDDest
+   FROM OldHRD.dbo.HRD) ot
+UNPIVOT
+   (
+	NumberOfCases For [Location]IN (FC5070, FC5702, FC5847, FC5687, FC5222, FC5222Dest, FCInTransit, FCXC, FCLX, FCLXDest, FCXD, FCXDDest)
+   )AS unpvt where NumberOfCases > 0;
+
+GO
+
+/*********************************************/
+/***** HRDDC  ********/
+/*********************************************/
+INSERT INTO dbo.HRDDC(HRDId, [Location], NumberOfCases)
+SELECT AutoNum, SUBSTRING([Location], 3, DATALENGTH([Location])- 2) as [Location], NumberOfCases
+FROM 
+   (SELECT AutoNum, DC5070, DC5702, DC5847, DC5687, DC5222,
+			CAST(SUBSTRING(DC5222Dest, PATINDEX('%[0-9]%', DC5222Dest), PATINDEX('%[0-9][^0-9]%', DC5222Dest + 't') - PATINDEX('%[0-9]%', 
+                    DC5222Dest) + 1) as int) AS DC5222Dest
+			,DCInTransit, DCXC, DCLX,
+			CAST(SUBSTRING(DCLXDest, PATINDEX('%[0-9]%', DCLXDest), PATINDEX('%[0-9][^0-9]%', DCLXDest + 't') - PATINDEX('%[0-9]%', 
+                    DCLXDest) + 1) as int) AS DCLXDest
+			,DCXD,
+			CAST(SUBSTRING(DCXDDest, PATINDEX('%[0-9]%', DCXDDest), PATINDEX('%[0-9][^0-9]%', DCXDDest + 't') - PATINDEX('%[0-9]%', 
+                    DCXDDest) + 1) as int) AS DCXDDest
+   FROM OldHRD.dbo.HRD) ot
+UNPIVOT
+   (
+	NumberOfCases For [Location]IN (DC5070, DC5702, DC5847, DC5687, DC5222, DC5222Dest, DCInTransit, DCXC, DCLX, DCLXDest, DCXD, DCXDDest)
+   )AS unpvt where NumberOfCases > 0;
+
+GO
+
+/*********************************************/
+/***** LaborCost  ********/
+/*********************************************/
+INSERT INTO [dbo].[LaborCost] (Year, LaborCost)
+	SELECT Year, LaborCost
+		FROM OldHRD.dbo.LaborCost
+
+GO
+
+/*********************************************/
+/***** PRODUCT  ********/
+/*********************************************/
+INSERT INTO [dbo].[Users] ([UserID], Name)
+	SELECT UserID,Name
+		FROM OldHRD.dbo.Users
+GO
+
+/*********************************************/
+/***** HRDNotes  ********/
+/*********************************************/
+SET IDENTITY_INSERT dbo.HRDNotes ON 
+INSERT INTO [dbo].[HRDNotes] ([Id], HRDId, [Category], [Description], UserID, [Date], [FileName], [Path], Size)
+	SELECT AutoNum, HRDNum, Category, [Description], UserID, [Date], [FileName], [Path], Size
+		FROM OldHRD.dbo.tblNotesList notes
+		WHERE notes.HRDNum in (select AutoNum from OldHRD.dbo.HRD)
+
+SET IDENTITY_INSERT dbo.HRDNotes OFF		
+GO
+
+/*********************************************/
+/***** Settings  ********/
+/*********************************************/
+SET IDENTITY_INSERT dbo.Settings ON 
+INSERT INTO [dbo].[Settings] ([ID], [Name], [Description], [Value], [Type])
+	SELECT [ID], [Name], [Description], [Value], [Type]
+		FROM OldHRD.dbo.tConstants
+
+SET IDENTITY_INSERT dbo.Settings OFF 
+GO
+
+/*********************************************/
+/***** Security  ********/
+/*********************************************/
+SET IDENTITY_INSERT dbo.[Security] ON 
+INSERT INTO [dbo].[Security]
+           ([ID], [Type], [Name], [Administrator], [QAManager], [QATGrp], [QAProdRelGrp], [ProdTLGrp], [ReworkMembersGrp],
+			[PhysicalGrp], [Physical5Grp], [GSTDNotificationGrp], [DeleteHRD], [EditHRDApproveRework], [ProdMaintenanceList],
+			[GSTDGrp], [BUMGrp], [LaborCostList], [Notification])
+	SELECT	SecurityID, [Type], [Name], [Administrator], [QAManager], [QATGrp], [QAProdRelGrp], [ProdTLGrp], [ReworkMembersGrp],
+			[PhysicalGrp], [Physical5Grp], [GSTDNotificationGrp], [DeleteHRD], [EditHRDApproveRework], [ProdMaintenanceList],
+			[GSTDGrp], [BUMGrp], [LaborCostList], [Notification]
+		   FROM OldHRD.dbo.tSecurity
+
+SET IDENTITY_INSERT dbo.[Security] OFF
+     
+GO
+
+/*********************************************/
+/***** TestCost  ********/
+/*********************************************/
+SET IDENTITY_INSERT dbo.TestCost ON 
+INSERT INTO [dbo].[TestCost] (Id, [Year], TestName, TestCost)
+	SELECT EntryID, [Year], TestName, TestCost
+		FROM OldHRD.dbo.tTestCost
+
+SET IDENTITY_INSERT dbo.TestCost OFF 
+GO
+
+/*********************************************/
+/***** HRDTestCost  ********/
+/*********************************************/
+SET IDENTITY_INSERT dbo.HRDTestCost ON 
+INSERT INTO [dbo].[HRDTestCost] ([Id], HRDId, TestName, Qty, Cost)
+	SELECT EntryID, HRDId, TestName, Qty, Cost
+		FROM OldHRD.dbo.tTestTracking trk
+		WHERE trk.HRDID in (select AutoNum from OldHRD.dbo.HRD)
+
+SET IDENTITY_INSERT dbo.HRDTestCost OFF		
+GO
+
+/*********************************************/
+/***** User  ********/
+/*********************************************/
+INSERT INTO [dbo].[Users] ([UserID], Name)
+	SELECT UserID,Name
+		FROM OldHRD.dbo.Users
+GO
+
+/*********************************************/
+/***** DROPDOWNTYPE  ********/
+/*********************************************/
+
+SET IDENTITY_INSERT [dbo].DropDownType ON 
+INSERT [dbo].DropDownType ([Id], [Name]) VALUES (1,  N'Category')
+INSERT [dbo].DropDownType ([Id], [Name]) VALUES (2,  N'SubCategory')
+INSERT [dbo].DropDownType ([Id], [Name]) VALUES (3,  N'Classification')
+INSERT [dbo].DropDownType ([Id], [Name]) VALUES (4,  N'Line')
+INSERT [dbo].DropDownType ([Id], [Name]) VALUES (5,  N'Shift')
+INSERT [dbo].DropDownType ([Id], [Name]) VALUES (6,  N'LCL')
+INSERT [dbo].DropDownType ([Id], [Name]) VALUES (7,  N'YesNo')
+INSERT [dbo].DropDownType ([Id], [Name]) VALUES (8,  N'YesNoNA')
+INSERT [dbo].DropDownType ([Id], [Name]) VALUES (9,  N'CodingType')
+INSERT [dbo].DropDownType ([Id], [Name]) VALUES (10, N'CodingDetails')
+
+SET IDENTITY_INSERT [dbo].DropDownType OFF 
+GO
+
+
+/*********************************************/
+/***** DROPDOWNTYPE  ********/
+/*********************************************/
+INSERT INTO [dbo].[DropDownItem] ([DropDownTypeId], [Value], SortOrder, IsActive)
+SELECT DISTINCT t.[Id] as DropDownTypeId ,PrimaryCategory as [Value], PrimarySort as SortOrder, 1 as IsActive
+FROM OldHRD.dbo.DDLs
+	JOIN [dbo].DropDownType t ON t.Name = 'Category'
+	WHERE PrimaryCategory is not null
+UNION ALL
+SELECT DISTINCT t.[Id] as DropDownTypeId, SubCategory as [Value], SubCategorySort as SortOrder, 1 as IsActive
+	FROM OldHRD.dbo.DDLs
+	JOIN [dbo].DropDownType t ON t.Name = 'SubCategory'
+	WHERE SubCategory is not null
+UNION ALL
+SELECT DISTINCT t.[Id] as DropDownTypeId, [Classification] as [Value], ClassificationSort as SortOrder, 1 as IsActive
+	FROM OldHRD.dbo.DDLs
+	JOIN [dbo].DropDownType t ON t.Name = 'Classification'
+	WHERE [Classification] is not null
+UNION ALL
+SELECT DISTINCT t.[Id] as DropDownTypeId, Line as [Value], LineSort as SortOrder, 1 as IsActive
+	FROM OldHRD.dbo.DDLs
+	JOIN [dbo].DropDownType t ON t.Name = 'Line'
+	WHERE Line is not null
+UNION ALL
+SELECT DISTINCT t.[Id] as DropDownTypeId, [Shift] as [Value], ShiftSort as SortOrder, ShiftActive as IsActive
+	FROM OldHRD.dbo.DDLs
+	JOIN [dbo].DropDownType t ON t.Name = 'Shift'
+	WHERE [Shift] is not null
+UNION ALL
+SELECT DISTINCT t.[Id] as DropDownTypeId, LCLLine as [Value], LCLSort as SortOrder, LCLActive as IsActive
+	FROM OldHRD.dbo.DDLs
+	JOIN [dbo].DropDownType t ON t.Name = 'LCL'
+	WHERE LCLLine is not null
+UNION ALL
+SELECT DISTINCT t.[Id] as DropDownTypeId, Schedule as [Value], ScheduleSort as SortOrder, ScheduleActive as IsActive
+	FROM OldHRD.dbo.DDLs
+	JOIN [dbo].DropDownType t ON t.Name = 'YesNo'
+	WHERE Schedule is not null
+UNION ALL
+SELECT DISTINCT t.[Id] as DropDownTypeId, YNNA as [Value], YNNASort as SortOrder, 1 as IsActive
+	FROM OldHRD.dbo.DDLs
+	JOIN [dbo].DropDownType t ON t.Name = 'YesNoNa'
+	WHERE YNNA is not null
+UNION ALL
+SELECT DISTINCT t.[Id] as DropDownTypeId, CodingType as [Value], CodingTypeSort as SortOrder, 1 as IsActive
+	FROM OldHRD.dbo.DDLs
+	JOIN [dbo].DropDownType t ON t.Name = 'CodingType'
+	WHERE CodingType is not null
+UNION ALL
+SELECT DISTINCT t.[Id] as DropDownTypeId, CodingDetails as [Value], CodingDetailsSort as SortOrder, 1 as IsActive
+	FROM OldHRD.dbo.DDLs
+	JOIN [dbo].DropDownType t ON t.Name = 'CodingDetails'
+	WHERE CodingDetails is not null
+
+GO
