29d8590f69fc51ed4de7e3b0537727a22799d8f9
Aleksey Glukharev <neogarrigus@gmail.com>
2022-06-09 15:05:24 +0200

    Set withCredentials to true and added CORS headers for OPTIONS/preflight requests

--- src/main.js
+++ src/main.js
@@ -7,7 +7,9 @@ import axios from 'axios'
 
 
 Vue.config.productionTip = false
-Vue.prototype.$axios = axios
+Vue.prototype.$axios = axios.create({
+    withCredentials: true
+});
 
 new Vue({
   router,
