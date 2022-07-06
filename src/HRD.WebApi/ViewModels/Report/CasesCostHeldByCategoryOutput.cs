095f1038dcd1475d048df6eb8c0e02bc630685e3
j-borbon <jborbon9@hotmail.com>
2022-07-06 21:13:45 +0800

    report api updates

--- src/HRD.WebApi/ViewModels/Report/CasesCostHeldByCategoryOutput.cs
+++ src/HRD.WebApi/ViewModels/Report/CasesCostHeldByCategoryOutput.cs
@@ -4,13 +4,8 @@ namespace HRD.WebApi.ViewModels.Report
 {
     public class CasesCostHeldByCategoryOutput
     {
-        public DateTime? MonthHeld { get; set; }
-        public string HoldCategory { get; set; }
         public int? TotalCases { get; set; }
         public decimal? TotalCost { get; set; }
         public string Line { get; set; }
-        public string YearHeld { get; set; }
-        public int? WeekHeld { get; set; }
-        public bool? IsComplete { get; set; }
     }
 }
---
+++ src/HRD.WebApi/ViewModels/ReportPaginationFilter.cs
@@ -0,0 +1,23 @@
+﻿namespace HRD.WebApi.ViewModels
+{
+    public class ReportPaginationFilter
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public string SortColumn { get; set; }
+        public string SortOrder { get; set; }
+        public ReportPaginationFilter()
+        {
+            this.PageNumber = 1;
+            this.PageSize = 10;
+        }
+
+        public ReportPaginationFilter(int pageNumber, int pageSize, string sortColumn, string sortOrder)
+        {
+            this.PageNumber = pageNumber < 1 ? 1 : pageNumber;
+            this.PageSize = pageSize < 10 ? 10 : pageSize;
+            this.SortColumn = !string.IsNullOrWhiteSpace(sortColumn) ? sortColumn.ToLower() : "";
+            this.SortOrder = !string.IsNullOrWhiteSpace(sortOrder) ? sortOrder.ToLower() : "asc";
+        }
+    }
+}
