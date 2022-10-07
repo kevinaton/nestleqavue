24666d79cf26408ad40ec1eb2fd0882414b74019
Jacqueline Borbon <jborbon9@hotmail.com>
2022-10-07 16:29:09 +0800

    added ef navigation for UserRole

--- src/HRD.WebApi/Data/Entities/UserRole.cs
+++ src/HRD.WebApi/Data/Entities/UserRole.cs
@@ -7,6 +7,7 @@ namespace HRD.WebApi.Data.Entities
     public partial class UserRole
     {
         public int UserId { get; set; }
+        public virtual User User { get; set; }
         public int RoleId { get; set; }
         public virtual Role Role { get; set; }
     }
