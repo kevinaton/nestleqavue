388220925f6bad97390ea8fb53d5bfb54b43a171
Jacqueline Borbon <jborbon9@hotmail.com>
2022-10-12 21:59:16 +0800

    create TestCosts api lookup, change Testcost to dropdown

--- src/HRD.WebApi/ViewModels/TestCostViewModel.cs
+++ src/HRD.WebApi/ViewModels/TestCostViewModel.cs
@@ -3,7 +3,7 @@
     public class TestCostViewModel
     {
         public int Id { get; set; }
-        public string Year { get; set; }
+        public int Year { get; set; }
         public string TestName { get; set; }
         public decimal? TestCost { get; set; }
     }
--- src/components/qa/Micro.vue
+++ src/components/qa/Micro.vue
@@ -255,18 +255,31 @@
                                     <v-col
                                         cols="12"
                                         sm="6"
-                                        md="4"
+                                        md="6"
                                     >
-                                        <v-text-field
+                                        <!-- <v-text-field
                                         v-model="testing.testName"
                                         placeholder="Enter here"
                                         label="Test Name"
-                                        ></v-text-field>
+                                        ></v-text-field> -->
+                                        <SelectTestCost
+                                            :inpValue="testing.testName"
+                                            :id="testing.id"
+                                            :access="!access.QARecordsEdit"
+                                            :cost="testing.cost"
+                                            :rules="rules"
+                                            :snackbar="snackbar"
+                                            label="Test Name" 
+                                            @change="(value, cost) => { 
+                                                testing.testName = value 
+                                                testing.cost = cost
+                                            }"
+                                        />
                                     </v-col>
                                     <v-col
                                         cols="12"
                                         sm="6"
-                                        md="4"
+                                        md="3"
                                     >
                                         <v-text-field
                                         v-model="testing.qty"
@@ -277,7 +290,7 @@
                                     <v-col
                                         cols="12"
                                         sm="6"
-                                        md="4"
+                                        md="3"
                                     >
                                         <v-text-field
                                         v-model="testing.cost"
@@ -337,16 +350,17 @@ import SimpleDatePicker from '@/components/FormElements/SimpleDatePicker.vue'
 import SnackBar from '@/components/TableElements/SnackBar.vue'
 import DeleteAction from '@/components/TableElements/DeleteAction.vue'
 import RowDelete from '@/components/TableElements/RowDelete.vue'
+import SelectTestCost from '@/components/FormElements/SelectTestCost.vue'
 
 export default {
     components: {
-        SelectDropdownString,
-        SimpleDatePicker,
-
-        SnackBar,
-        DeleteAction,
-        RowDelete,
-    },
+    SelectDropdownString,
+    SimpleDatePicker,
+    SnackBar,
+    DeleteAction,
+    RowDelete,
+    SelectTestCost
+},
     props: {
         name:'Micro',
         input: {
