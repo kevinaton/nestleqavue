d967c629c8e288160f636afcd0126773440f04d7
Kevin Aton <kevinaton@Kevins-MacBook-Pro.local>
2022-11-04 22:39:03 +0800

    uncommented controllers

--- src/HRD.WebApi/Controllers/HrdsController.cs
+++ src/HRD.WebApi/Controllers/HrdsController.cs
@@ -35,7 +35,7 @@ namespace HRD.WebApi.Controllers
 
         // GET: api/Hrds
         [HttpGet]
-        // [Authorize(Policy = PolicyNames.ViewHRDs)]
+        [Authorize(Policy = PolicyNames.ViewHRDs)]
         public async Task<ActionResult<IEnumerable<QAListViewModel>>> GetHrds([FromQuery] HrdPaginationFilter filter)
         {
             var validFilter = new HrdPaginationFilter(filter.PageNumber, filter.PageSize, filter.SortColumn, filter.SortOrder, filter.SearchString, filter.FilterCriteria);
@@ -170,7 +170,7 @@ namespace HRD.WebApi.Controllers
 
         // GET: api/Hrds/5
         [HttpGet("Hrd/{id}")]
-        // [Authorize(Policy = PolicyNames.ViewHRDs)]
+        [Authorize(Policy = PolicyNames.ViewHRDs)]
         public async Task<ActionResult<HRDDetailViewModel>> GetHrd(int id)
         {
             var hrd = await _context.Hrds.Include(i => i.Hrddcs)
@@ -288,7 +288,7 @@ namespace HRD.WebApi.Controllers
         // PUT: api/Hrds/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("Hrd/{id}")]
-        // [Authorize(Policy = PolicyNames.EditHRDs)]
+        [Authorize(Policy = PolicyNames.EditHRDs)]
         public async Task<IActionResult> PutHrd(int id, [FromForm] string jsonString, [FromForm] List<IFormFile> files)
         {
             var model = JsonConvert.DeserializeObject<HRDDetailViewModel>(jsonString);
@@ -544,7 +544,7 @@ namespace HRD.WebApi.Controllers
         // POST: api/Hrds
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost("Hrd")]
-        // [Authorize(Policy = PolicyNames.EditHRDs)]
+        [Authorize(Policy = PolicyNames.EditHRDs)]
         public async Task<ActionResult<HRDDetailViewModel>> PostHrd(HRDDetailViewModel model)
         {
             var hrd = new Hrd
@@ -653,7 +653,7 @@ namespace HRD.WebApi.Controllers
 
         // DELETE: api/Hrds/5
         [HttpDelete("Hrd/{id}")]
-        // [Authorize(Policy = PolicyNames.EditHRDs)]
+        [Authorize(Policy = PolicyNames.EditHRDs)]
         public async Task<IActionResult> DeleteHrd(int id)
         {
             var hrd = await _context.Hrds
@@ -700,7 +700,7 @@ namespace HRD.WebApi.Controllers
 
         // GET: api/Hrds/5
         [HttpGet("Qa/{id}")]
-        // [Authorize(Policy = PolicyNames.ViewHRDs)]
+        [Authorize(Policy = PolicyNames.ViewHRDs)]
         public async Task<ActionResult<QARecordViewModel>> GetQARecord(int id)
         {
             var qa = await _context.Hrds.Include(i => i.HrdtestCosts)
@@ -807,7 +807,7 @@ namespace HRD.WebApi.Controllers
         // PUT: api/Hrds/Qa/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("Qa/{id}")]
-        // [Authorize(Policy = PolicyNames.EditHRDs)]
+        [Authorize(Policy = PolicyNames.EditHRDs)]
         public async Task<IActionResult> PutQARecord(int id, [FromForm] string jsonString, [FromForm] List<IFormFile> files)
         {
             var model = JsonConvert.DeserializeObject<QARecordViewModel>(jsonString);
@@ -1062,7 +1062,7 @@ namespace HRD.WebApi.Controllers
         // POST: api/Hrds
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost("Qa")]
-        // [Authorize(Policy = PolicyNames.EditHRDs)]
+        [Authorize(Policy = PolicyNames.EditHRDs)]
         public async Task<ActionResult<QARecordViewModel>> PostQARecord(QARecordViewModel model)
         {
             var hrd = new Hrd
@@ -1149,7 +1149,7 @@ namespace HRD.WebApi.Controllers
 
         // DELETE: api/Hrds/5
         [HttpDelete("Qa/{id}")]
-        // [Authorize(Policy = PolicyNames.EditHRDs)]
+        [Authorize(Policy = PolicyNames.EditHRDs)]
         public async Task<IActionResult> DeleteQARecord(int id)
         {
             var hrd = await _context.Hrds
@@ -1190,7 +1190,7 @@ namespace HRD.WebApi.Controllers
         }
 
         [HttpPost("Recalculate")]
-        // [Authorize(Policy = PolicyNames.EditHRDs)]
+        [Authorize(Policy = PolicyNames.EditHRDs)]
         public ActionResult<RecalculateViewModel> Recalculate(RecalculateViewModel model)
         {
             var recalculate = model.Clear + model.Sample + model.Scrap + model.ThriftStore + model.Donate;
